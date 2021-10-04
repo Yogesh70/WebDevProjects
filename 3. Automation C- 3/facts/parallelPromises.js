a4c3fd355f23d12da41adddc694418743a636fca
Yogesh70 <66455133+Yogesh70@users.noreply.github.com>
2021-10-04 20:41:51 +0530

    parallelPromises syntax symplified C- 3

--- 3. Automation C- 3/facts/parallelPromises.js
+++ 3. Automation C- 3/facts/parallelPromises.js
@@ -8,27 +8,23 @@ let f2PendingPromise = fs.promises.readFile('./f2.txt');
 let f3PendingPromise = fs.promises.readFile('./f3.txt');
 
 
-f1PendingPromise.then(function (data) {
-    console.log('Content ' + data);
-});
-f1PendingPromise.catch(function (error) {
-    console.log(error);
-});
+f1PendingPromise.then(scb);
+f1PendingPromise.catch(fcb);
 
 
-f2PendingPromise.then(function (data) {
-    console.log('Content ' + data);
-});
-f2PendingPromise.catch(function (error) {
-    console.log(error);
-});
+f2PendingPromise.then(scb);
+f2PendingPromise.catch(fcb);
 
 
-f3PendingPromise.then(function (data) {
-    console.log('Content ' + data);
-});
-f3PendingPromise.catch(function (error) {
-    console.log(error);
-});
+f3PendingPromise.then(scb);
+f3PendingPromise.catch(fcb);
 
 console.log('After');
+
+function scb(data) {
+    console.log('Content ' + data);
+}
+
+function fcb(error) {
+    console.log(error);
+}
