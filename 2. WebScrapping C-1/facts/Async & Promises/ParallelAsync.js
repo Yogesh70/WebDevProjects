f0a2212d2d03a0907e843186b21d715e9cc8c14e
Yogesh70 <66455133+Yogesh70@users.noreply.github.com>
2021-10-04 10:50:02 +0530

    Async JS types & working C- 3

---
+++ 2. WebScrapping C-1/facts/Async & Promises/ParallelAsync.js
@@ -0,0 +1,18 @@
+// async tasks, three files, parallely read file
+let fs = require('fs');
+
+console.log('Before');
+
+fs.readFile('./f1.txt', function cb(error, data) {
+    console.log('Content ' + data);
+})
+
+fs.readFile('./f2.txt', function cb(error, data) {
+    console.log('Content ' + data);
+})
+
+fs.readFile('./f3.txt', function cb(error, data) {
+    console.log('Content ' + data);
+})
+
+console.log('After');
---
+++ 2. WebScrapping C-1/facts/Async & Promises/SerialAsync.js
@@ -0,0 +1,19 @@
+// async, three files, serially read file
+let fs = require('fs');
+
+console.log('Before');
+
+fs.readFile('./f1.txt', function cb(error, data) {
+    console.log('Content ' + data);
+
+    fs.readFile('./f2.txt', function cb(error, data) {
+        console.log('Content ' + data);
+
+        fs.readFile('./f3.txt', function cb(error, data) {
+            console.log('Content ' + data);
+        })
+    })
+})
+
+
+console.log('After');
