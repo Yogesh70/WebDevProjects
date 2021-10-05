55de0740f4e8cc9f9a2c78a68c92519f520895d5
Yogesh70 <66455133+Yogesh70@users.noreply.github.com>
2021-10-05 17:24:00 +0530

    Got all links of questions C- 4

--- 3. Automation C- 3/project/HackerRank/login.js
+++ 3. Automation C- 3/project/HackerRank/login.js
@@ -34,6 +34,37 @@ browserOpenPromise.then(function (browser) {
         let loginBtnClicked = tab.click('.ui-btn.ui-btn-large.ui-btn-primary.auth-button.ui-btn-styled');
         return loginBtnClicked;
     })
+    .then(function () {
+        // On change of webPage, Data comes up very fast but DOM is not loaded till yet. So, Time is reqd for loading of DOM. To overCome this problem we use waitPromise so that DOM is loaded completely.  
+        let waitPromise = tab.waitForSelector('a[data-attr1="one-month-preparation-kit"]', { visible: true })
+        return waitPromise;
+    })
+    .then(function () {
+        let PrepkitBtnPromise = tab.click('a[data-attr1="one-month-preparation-kit"]');
+        return PrepkitBtnPromise;
+    })
+    .then(function () {
+        let waitPromise = tab.waitForSelector('.ui-btn.ui-btn-normal.ui-btn-line-primary.interview-ch-li-cta.ui-btn-link.ui-btn-styled');
+        return waitPromise;
+    })
+    .then(function () {            // document.querySelectorAll() -> tab.$$() in Puppeteer
+        let allQuestionsPromise = tab.$$('.ui-btn.ui-btn-normal.ui-btn-line-primary.interview-ch-li-cta.ui-btn-link.ui-btn-styled');
+        return allQuestionsPromise;
+    })
+    .then(function (allQuestions) {
+        // [ <a> </a> , <a> </a> , <a> </a> ]
+        let allLinksPromise = [];
+        for (let i = 0; i < allQuestions.length; i++) {
+            let linkPendingPromise = tab.evaluate(function (elem) { return elem.getAttribute('href'); }, allQuestions[i]);
+            allLinksPromise.push(linkPendingPromise);
+        }
+
+        let allQuestionsPromise = Promise.all(allLinksPromise);
+        return allQuestionsPromise;
+    })
+    .then(function (allLinks) {
+        console.log(allLinks);
+    })
     .catch(function (error) {
         console.log(error);
     })
