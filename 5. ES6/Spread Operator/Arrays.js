6322dfd38d3e47035218f16cb0215f53719fa2f0
Yogesh70 <66455133+Yogesh70@users.noreply.github.com>
2021-10-11 17:34:14 +0530

    Spread Operator: Arrays & Objects

---
+++ 5. ES6/Spread Operator/Arrays.js
@@ -0,0 +1,13 @@
+// ... => Spread Operator
+
+// let arr = [1, 2, 3, 4, 5, 6];
+// console.log(...arr);
+
+let arr1 = [1, 2, 3, 4];
+let arr2 = [6, 7, 8];
+// concatenate these 2 arrays
+// [1,2,3,4,5,6,7,8]
+// new array
+
+let nArr = [...arr1, 5, ...arr2];
+console.log(nArr);
---
+++ 5. ES6/Spread Operator/Objects.js
@@ -0,0 +1,61 @@
+const { stat } = require("fs");
+
+let state = {
+    name: 'John',
+    address: {
+        city: "London",
+        country: {
+            countryName: "United Kingdom",
+            countryCode: "UK"
+        }
+    }
+}
+
+// arrays, objects & functions -> Heap Memory Area
+// Ref Copy -> Change in state as well as copy
+// let copy = state;
+// copy.name = "Yogesh";
+// console.log(state);
+// console.log(copy);
+
+// using spread operator
+let copy = { ...state }; // shallow copy
+// copy.name = "Yogesh";
+// console.log(state);
+// console.log(copy);
+
+// copy.address.city = "Delhi"; // change in both copy & state
+// console.log(state);
+// console.log(copy);
+
+// shallow copy for an object i.e. spread, the uppermost level is created at a new memory space 
+// the properties of the uppermost level remains the same
+// the lower levels are not affected by this... they keep on pointing to their original reference
+
+let doubleCopy = {
+    ...state,
+    address: {
+        ...state.address
+    }
+}
+// doubleCopy.address.city = "Delhi";
+// console.log(state);
+// console.log(doubleCopy);
+
+// doubleCopy.address.country.countryName = "India";
+// console.log(state);
+// console.log(doubleCopy);
+
+
+let deepCopy = {
+    ...state,
+    address: {
+        ...state.address,
+        country: {
+            ...state.address.country
+        }
+    }
+}
+deepCopy.address.country.countryName = "India";
+console.log(state);
+console.log(deepCopy);
