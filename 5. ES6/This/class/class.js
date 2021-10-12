a202f2d51f590a7dfd25268ed16ccacb2503b9b5
Yogesh70 <66455133+Yogesh70@users.noreply.github.com>
2021-10-12 20:07:51 +0530

    Using Arrow fn to overcome the problem

--- 5. ES6/This/class/class.js
+++ 5. ES6/This/class/class.js
@@ -27,24 +27,24 @@
 
 ///////////////////////////// Bind
 
-class abc {
-    constructor(person) {
-        this.person = person;
-        this.age = 25;
-        this.sayHi = this.sayHi.bind(this);
-    }
+// class abc {
+//     constructor(person) {
+//         this.person = person;
+//         this.age = 25;
+//         this.sayHi = this.sayHi.bind(this);
+//     }
 
-    sayHi() {
-        console.log('Hello');
-        console.log(this.person);
-        // console.log(this);
-    }
-}
+//     sayHi() {
+//         console.log('Hello');
+//         console.log(this.person);
+//         // console.log(this);
+//     }
+// }
 
 // member func. are made on heap at common location for different obj.
 
-let obj = new abc('Alex');
-let obj2 = new abc('Stuart');
+// let obj = new abc('Alex');
+// let obj2 = new abc('Stuart');
 // console.log(obj);
 // console.log(obj2);
 
@@ -52,6 +52,32 @@ let obj2 = new abc('Stuart');
 // fn();   
 
 // problem
-let btn = document.querySelector('button');
+// let btn = document.querySelector('button');
 // btn.addEventListener('click', obj.sayHi);
+// btn.addEventListener('click', obj.sayHi);
+
+///////////////////////////// Arrow Fn
+
+class abc {
+    constructor(person) {
+        this.person = person;
+        this.age = 25;
+    }
+    sayHi = () => {
+        console.log('Hello');
+        console.log(this.person);
+        // console.log(this);
+    }
+}
+
+let obj = new abc('Alex');
+let obj2 = new abc('Stuart');
+console.log(obj);
+// console.log(obj2);
+
+// let fn = obj.sayHi;
+// fn();   
+
+// problem
+let btn = document.querySelector('button');
 btn.addEventListener('click', obj.sayHi);
