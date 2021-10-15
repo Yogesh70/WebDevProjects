e80e8abb2283fe3d490bed640473c153417661e5
Yogesh70 <66455133+Yogesh70@users.noreply.github.com>
2021-10-15 09:54:12 +0530

    Started React Routing C- 4

--- 6. React JS/moviesapp/src/App.js
+++ 6. React JS/moviesapp/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Movies from "./Components/Movies";
+import About from "./Components/About";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <React.Fragment>
+      <Home />
       <Movies />
+      <About />
     </React.Fragment>
   );
 }
