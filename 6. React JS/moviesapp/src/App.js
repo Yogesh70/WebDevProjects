a1735ebc91a12df3397fcf120cd7a9366626a103
Yogesh70 <66455133+Yogesh70@users.noreply.github.com>
2021-10-15 11:20:05 +0530

    Routing Done

--- 6. React JS/moviesapp/src/App.js
+++ 6. React JS/moviesapp/src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import Movies from "./Components/Movies";
 import About from "./Components/About";
 import Home from "./Components/Home";
+import Nav from "./Components/Nav";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <React.Fragment>
-      <Home />
-      <Movies />
-      <About />
+      <Router>  {/* Provides the functionality of Routing in React */}
+        <Nav />
+        <Switch>  {/* Checks the path with the route entered */}
+          <Route path='/' exact component={Home} /> {/* exact -> Checks the exact path with the route entered */}
+          <Route path='/movies' exact component={Movies} />
+          <Route path='/about' exact component={About} />
+        </Switch>
+      </Router>
     </React.Fragment>
   );
 }
