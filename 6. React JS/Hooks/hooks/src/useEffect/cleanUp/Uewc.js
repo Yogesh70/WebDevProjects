c9d9f6b0c0b76013d472f0f66677d220c968419d
Yogesh70 <66455133+Yogesh70@users.noreply.github.com>
2021-10-16 23:03:52 +0530

    cleanUp without passing dependency array

---
+++ 6. React JS/Hooks/hooks/src/useEffect/cleanUp/Uewc.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from 'react';
+
+function Uewc() {
+    const [count, setCount] = useState(0);
+    console.log('render');
+
+    useEffect(() => {
+        console.log('useEffect');
+        document.title = `Clicked ${count} times`;
+        // cleanUp optional -> runs before next useEffect() Hook runs
+        return () => {
+            alert(`I will be called before the next useEffect is called ${count}`);
+        }
+    })
+    return (
+        <div>
+            <p>{count}</p>
+            <button onClick={() => { setCount(count + 1) }}>Click</button>
+        </div>
+    );
+}
+
+export default Uewc;
