6c1624eafc9ab5ee986b899d650b8a4bea7cf015
Yogesh70 <66455133+Yogesh70@users.noreply.github.com>
2021-10-20 12:56:08 +0530

    auth updated

--- 6. React JS/firebase/src/Components/Demo.js
+++ 6. React JS/firebase/src/Components/Demo.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import firebase from './firebase';
 
 function Demo() {
-    console.log(firebase);
+    // console.log(firebase);
     const auth = firebase.auth();
     const [user, setUser] = useState(null);
     const [email, setEmail] = useState('');
@@ -21,26 +21,41 @@ function Demo() {
             setLoading(false);
         }
         catch (error) {
-            setError('Failed to sign in');
+            setError(error.message);
             setTimeout(() => {
                 setError('');
             }, 2000)
             setLoading(false);
         }
+        setEmail('');
+        setPassword('');
+    }
+
+    const handleSignOut = () => {
+
     }
 
     return (
-        <div>
-            <label>
-                Email:
-                <input type='text' value={email} onChange={(e) => { setEmail(e.target.value) }}></input>
-            </label>
-            <label>
-                Password:
-                <input type='password' value={password} onChange={(e) => { setPassword(e.target.value) }}></input>
-            </label>
-            <button onClick={handleSubmit}>SignIn</button>
-        </div>
+        <>
+            {loading ? <h1>Please Wait.....Loading</h1> : user == null ?
+                < div >
+                    <label>
+                        Email:
+                        <input type='text' value={email} onChange={(e) => { setEmail(e.target.value) }}></input>
+                    </label>
+                    <label>
+                        Password:
+                        <input type='password' value={password} onChange={(e) => { setPassword(e.target.value) }}></input>
+                    </label>
+                    <button onClick={handleSubmit}>SignIn</button>
+                    {error ? <h1>{error}</h1> : <> </>}
+                </div> :
+                <>
+                    <h2>{user.uid} is Signed In</h2>
+                    <button onClick={handleSignOut}>Sign Out</button>
+                </>
+            }
+        </>
     );
 }
 
